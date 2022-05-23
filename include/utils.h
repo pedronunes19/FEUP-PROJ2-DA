2b93909159fa0178da04da2d51e09f25515eac7c
toni-santos <a.oliveirasantos122@gmail.com>
2022-05-23 17:37:09 +0100

    added utility functions

--- include/utils.h
+++ include/utils.h
@@ -1,10 +1,23 @@
-#include <string>
-#include <vector>
-
+#pragma once
 #ifndef DA_PROJ_2_UTILS_H
 #define DA_PROJ_2_UTILS_H
 
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <limits>
+
+
+#define MAX_CHARS_IGNORE std::numeric_limits <std::streamsize>::max()
+
 namespace utils::file {
+
+    /**
+     * @brief Clears the screen
+     *
+     */
+    void clearScreen();
 
     /**
      * @brief Reads a file at the specified path and returns its content as a collection of its lines.
@@ -14,6 +27,33 @@ namespace utils::file {
      * @return a vector containing all the lines but the first of the given file
      */
     std::vector<std::string> readFile(const std::string& filePath);
+
+    /**
+     * @brief Splits a string by a character.
+     *
+     * @param str The string to be split.
+     * @param i The character to use as the separator.
+     * @return A vector with all the words in the string that were separated by the
+     *         given character.
+     */
+    std::vector<std::string> split(std::string str, char sep);
+
+    /**
+     * @brief Waits for the user to press ENTER
+     *
+     * @param prompt the prompt to show, defaults to "Press ENTER to continue..."
+     */
+    void waitForEnter(std::string prompt = "Press ENTER to continue...");
+
+    /**
+     * @brief Normalizes a string.
+     *
+     * @details Replaces all tabs with spaces, removes duplicate spaces and trims
+     *          spaces from the start and the end.
+     *
+     * @param input The string to normalize.
+     */
+    void normalizeInput(std::string &input);
 }
 
 #endif //DA_PROJ_2_UTILS_H
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,6 +1,6 @@
-#include <fstream>
-
 #include "../include/utils.h"
+
+void utils::file::clearScreen() {std::cout << std::string(500, '\n') << std::endl;};
 
 std::vector<std::string> utils::file::readFile(const std::string& filePath) {
 
@@ -18,4 +18,52 @@ std::vector<std::string> utils::file::readFile(const std::string& filePath) {
         lines.push_back(line);
 
     return lines;
+}
+
+std::vector<std::string> utils::file::split(std::string str, char sep) {
+    std::string temp;
+    std::vector<std::string> final;
+
+    for (auto i : str) {
+        if (i == sep) {
+            final.push_back(temp);
+            temp = "";
+        } else {
+            temp += i;
+        }
+    }
+    final.push_back(temp);
+
+    return final;
+}
+
+void utils::file::waitForEnter(std::string prompt) {
+    if (std::cin.good()) std::cout << prompt << std::endl;
+    if (std::cin.peek() == '\n') std::cin.ignore(MAX_CHARS_IGNORE, '\n');
+}
+
+void utils::file::normalizeInput(std::string &input) {
+    char last = 0;
+    size_t i = 0;
+    while (i < input.length()) {
+        char &c = input.at(i);
+
+        // Replace tabs with spaces
+        if (c == '\t')
+            c = ' ';
+
+        // Delete character if it is a space at the begining, at the end or
+        // after another space
+        if (c == ' ' &&
+            (last == ' ' || last == 0 || i == input.length() - 1 || i == 0)) {
+            input.erase(i, 1);
+
+            // If we're outside the string, go back one
+            if (i == input.length())
+                i--;
+        } else {
+            i++;
+            last = c;
+        }
+    }
 }
