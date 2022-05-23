3b76299d439c10227694563c637a6e0f70587467
toni-santos <a.oliveirasantos122@gmail.com>
2022-05-23 17:34:37 +0100

    added some constants

---
+++ include/constants.h
@@ -0,0 +1,17 @@
+#ifndef DA_PROJ_1_CONSTANTS_H
+#define DA_PROJ_1_CONSTANTS_H
+
+#include <string>
+
+const std::string NORMAL_DATASET_1 = "../resources/Tests/in01.txt";
+const std::string NORMAL_DATASET_2 = "../resources/Tests/in02.txt";
+const std::string NORMAL_DATASET_3 = "../resources/Tests/in03.txt";
+const std::string NORMAL_DATASET_4 = "../resources/Tests/in04.txt";
+const std::string NORMAL_DATASET_5 = "../resources/Tests/in05.txt";
+const std::string NORMAL_DATASET_6 = "../resources/Tests/in06.txt";
+const std::string NORMAL_DATASET_7 = "../resources/Tests/in07txt";
+const std::string NORMAL_DATASET_8 = "../resources/Tests/in08.txt";
+const std::string NORMAL_DATASET_9 = "../resources/Tests/in09.txt";
+const std::string NORMAL_DATASET_10 = "../resources/Tests/in10.txt";
+
+#endif //DA_PROJ_1_CONSTANTS_H
